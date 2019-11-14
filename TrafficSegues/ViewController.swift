4678aa39c1180bd2811c328d5eab7d363ac2565f
Jeewoo Lee <jlee@lcs.on.ca>
2019-11-13 19:18:01 -0500

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Lee, Jeewoo on 2019-11-13.
+//  Copyright © 2019 Lee, Jeewoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
